cfd3cacfc7145e8bfd31031d79f0150a9d9fb50f
Gloria Bradford <31865225+shinyb@users.noreply.github.com>
2022-09-04 18:00:37 -0500

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export default [
         id: 1,
         title: "Missouri Botanical Garden",
         location: "St. Louis, Missouri",
-        googleMapsUrl: "https://goo.gl/maps/zJwqdFYNLdEjomdh6",
+        googleMapsUrl: https://goo.gl/maps/fZzmsm7DPPhqEo1TA,
         startDate: "25 May, 2022",
         endDate: "25 May, 2022",
         description: " The Garden is a center for botanical research and science education, as well as an oasis in the city of St. Louis. The Garden offers 79 acres of beautiful horticultural display, including a 14-acre Japanese strolling garden, historic architecture, and one of the world's largest collections of rare and endangered flora.",
