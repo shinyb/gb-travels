86686d536fd5fea44ba37d7aff62de84ab95f59f
Gloria Bradford <gloria_bradford2@yahoo.com>
2024-01-09 22:33:30 -0600

    Update index.pack.js

--- index.pack.js
+++ index.pack.js
@@ -577,34 +577,35 @@ function Posts(props) {
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = [{
-    id: 1,
-    title: "Missouri Botanical Garden",
-    location: "St. Louis, Missouri",
-    googleMapsUrl: "https://goo.gl/maps/zJwqdFYNLdEjomdh6",
-    startDate: "25 May, 2022",
-    endDate: "25 May, 2022",
-    description: " The Garden is a center for botanical research and science education, as well as an oasis in the city of St. Louis. The Garden offers 79 acres of beautiful horticultural display, including a 14-acre Japanese strolling garden, historic architecture, and one of the world's largest collections of rare and endangered flora.",
-    imageUrl: "mo-botanicalgarden.jpg"
+  id: 1,
+  title: "Missouri Botanical Garden",
+  location: "St. Louis, Missouri",
+  googleMapsUrl: "https://goo.gl/maps/LgA42rpB2yGkausY8",
+  startDate: "25 May, 2022",
+  endDate: "25 May, 2022",
+  description: " The Garden is a center for botanical research and science education, as well as an oasis in the city of St. Louis. The Garden offers 79 acres of beautiful horticultural display, including a 14-acre Japanese strolling garden, historic architecture, and one of the world's largest collections of rare and endangered flora.",
+  imageUrl: "mo-botanicalgarden.jpg"
 }, {
-    id: 2,
-    title: "Morton Arboretum",
-    location: "Lisle, Illinois",
-    googleMapsUrl: "https://goo.gl/maps/V7jK8ia1AtoP2JLk6",
-    startDate: "9 Aug, 2019",
-    endDate: "11 Aug, 2019",
-    description: "'Heartwood' is one of the five sculptures installed at various locations at the Morton Arboretum. The Arboretum features plant collections and gardens amid natural landscapes of woodlands, prairie, lakes, and streams. Its living collections include 222,000 plant specimens representing 4,650 different kinds of plants.",
-    imageUrl: "split-sculture.jpg"
-}, { id: 3,
-    title: "Taste of Chicago",
-    location: "Chicago, Illinois",
-    googleMapsUrl: "https://goo.gl/maps/jhvvaAm6ZG9X5Uzf9",
-    startDate: "3 Jul, 2018",
-    endDate: "12 Jul, 2018",
-    description: " The Taste of Chicago (also known locally as The Taste) is the world's largest food festival, held for five days in July in Chicago, Illinois in Grant Park. The event is also the largest festival in Chicago.",
-    imageUrl: "taste4.jpg"
+  id: 2,
+  title: "Morton Arboretum",
+  location: "Lisle, Illinois",
+  googleMapsUrl: "https://maps.app.goo.gl/G4BcuEuJJFKR9bri9",
+  startDate: "9 Aug, 2019",
+  endDate: "11 Aug, 2019",
+  description: "'Heartwood' is one of the five sculptures installed at various locations at the Morton Arboretum. The Arboretum features plant collections and gardens amid natural landscapes of woodlands, prairie, lakes, and streams. Its living collections include 222,000 plant specimens representing 4,650 different kinds of plants.",
+  imageUrl: "split-sculture.jpg"
+}, {
+  id: 3,
+  title: "Taste of Chicago",
+  location: "Chicago, Illinois",
+  googleMapsUrl: "https://goo.gl/maps/jhvvaAm6ZG9X5Uzf9",
+  startDate: "3 Jul, 2018",
+  endDate: "12 Jul, 2018",
+  description: " The Taste of Chicago (also known locally as The Taste) is the world's largest food festival, held for five days in July in Chicago, Illinois in Grant Park. The event is also the largest festival in Chicago.",
+  imageUrl: "taste4.jpg"
 }];
 
 /***/ }),
